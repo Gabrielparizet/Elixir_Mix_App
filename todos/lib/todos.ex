67bd82ee2e79a4955d0ca69d83830f9003752852
Gabrielparizet <parizetgabriel@gmail.com>
2023-08-24 18:13:02 +0200

    Enum Module

--- todos/lib/todos.ex
+++ todos/lib/todos.ex
@@ -1,4 +1,5 @@
 defmodule Todos do
+  # todo - complete later
   def create_todos do
     # old code - not so nice
     # number_of_tasks = IO.gets("Enter the number of todos you want to add: ") # {3, "\n"}
@@ -6,7 +7,21 @@ defmodule Todos do
     # number_of_tasks
     # cool code - pipe operator
     # The pipe operator |> passes the result of an expression as the first parameter of another expression.
-    {number_of_tasks, _} = IO.gets("Enter the number of todos you want to add: ") |> Integer.parse()
+    {number_of_tasks, _} =
+      IO.gets("Enter the number of todos you want to add: ") |> Integer.parse()
     number_of_tasks
+  end
+
+  def temp_todos do
+    ["eat apple", "read book", "elixir series", "exercises"]
+  end
+
+  def contains?(tasks, task) do
+    Enum.member?(tasks, task)
+  end
+
+  def random_task(tasks) do
+    [task] = Enum.take_random(tasks, 1)
+    task
   end
 end
